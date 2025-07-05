b4d5296bc9c5c2f95c9e5db9a3dc61bbd4530038
Rodrigo M.V.S. <88489374+RodrigoMvs123@users.noreply.github.com>
2025-07-05 09:33:22 -0300

    Removing hard coded API KEY and TOKEN

--- mini-qa.py
+++ mini-qa.py
@@ -1,9 +1,9 @@
 ASTRA_DB_BUNDLE_PATH="C:\Users\Matheus\Desktop\Rodrigo\Visual Studio Code\Ania Kubow\Vector Embeddings for Beginners - 30 min CS Course  OpenAI\search-python\secure-connect-vector-database.zip"
-ASTRA_DB_APPLICATION_TOKEN="AstraCS:zOhFuwhgurjebZbUwQoNKZxB:1327a178849314f6dc31d32e84de44e9bcb2abeed1105317c18382b6426c02b6"
+ASTRA_DB_APPLICATION_TOKEN="AstraCS:....."
 ASTRA_DB_CLIENT_ID=""
 ASTRA_DB_CLIENT_SECRETE="" 
 ASTRA_DB_KEYSPACE="search"
-OPEN_AI_KEY="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"
+OPEN_AI_KEY="....."
 
 from langchain.vectorstores.cassandra import Cassandra
 from langchain.indexes.vectorstore import VectorStoreIndexWrapper
